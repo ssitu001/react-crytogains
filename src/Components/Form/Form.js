caf3351dc024d93793f30c3eaddbaa64f0d1e2d0
Simon Situ <ssitu001@gmail.com>
2017-12-16 08:55:19 -0800

    add addtional form validation and logic

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,29 +1,101 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Form, Dropdown, Button } from 'semantic-ui-react';
 import { coins, coinOpts } from '../../coins.js';
 
-const FormComponent = () => (
-  <Form>
-    <Form.Group>
-      <Form.Field width={4}>
-        <label>Coin</label>
-        <Dropdown fluid search selection options={coinOpts} placeholder='Select Coin' />
-      </Form.Field>
-      <Form.Field width={4}>
-        <label>Cost Basis</label>
-        <Form.Input placeholder='$0' />
-      </Form.Field>
-      <Form.Field width={4}>
-        <label>Quantity</label>
-        <Form.Input placeholder='0.00'/>
-      </Form.Field>
-      <Form.Field width={4}>
-        <label>Date Acquired</label>
-        <Form.Input placeholder='MM/DD/YYYY'/>
-      </Form.Field>
-    </Form.Group>
-    <Form.Field control={Button}>Add</Form.Field>
-  </Form>
-)
+import './Form.css';
+
+class FormComponent extends Component {
+  state = {
+    coin: '',
+    costBasis: '',
+    quantity: '',
+    dateAcquired: '',
+  }
+
+  handleSubmit = (e) => {
+    //disptatch some action
+    e.preventDefault();
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleDropDownChange = (e, propsObj) => {
+    const { name, value } = propsObj;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  componentDidUpdate() {
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  showRequiredStar(inputField) {
+    return this.state[inputField].length > 0
+    ? null
+    : <span className="required-field">*</span>;
+  };
+
+  render() {
+    return (
+      <Form>
+        <Form.Group>
+          <Form.Field width={4}>
+            <label>Coin {this.showRequiredStar('coin')}</label>
+            <Dropdown 
+              fluid 
+              search 
+              selection 
+              options={coinOpts}
+              onChange={this.handleDropDownChange}
+              name='coin'
+              placeholder='Select Coin' 
+            />
+          </Form.Field>
+          <Form.Field width={4}>
+            <label>Cost Basis {this.showRequiredStar('costBasis')}</label>
+            <Form.Input
+              onChange={this.handleChange}
+              name='costBasis'
+              // value={'$'} 
+              placeholder='$0'/>
+          </Form.Field>
+          <Form.Field width={4}>
+            <label>Quantity {this.showRequiredStar('quantity')}</label>
+            <Form.Input 
+              type='number' 
+              min="1"
+              onChange={this.handleChange}
+              name='quantity'
+              placeholder='ex 2.77'
+            />
+          </Form.Field>
+          <Form.Field width={4}>
+            <label>Date Acquired</label>
+            <Form.Input
+              input='date'
+              onChange={this.handleChange}
+              name='dateAcquired' 
+            />
+          </Form.Field>
+        </Form.Group>
+        <Form.Field 
+          control={Button}
+          onClick={this.handleSubmit}>
+          Add Transaction
+        </Form.Field>
+      </Form>
+    )
+  }
+}
+
 
 export default FormComponent;
