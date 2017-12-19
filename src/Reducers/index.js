a54e7f7d6f7be1de28940183b60df4269a290c12
Simon Situ <ssitu001@gmail.com>
2017-12-18 16:41:14 -0800

    add get prices redcuers

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import handleTransaction from './handleTransaction';
+import getPrices from './getPrices';
 
 const rootReducer = combineReducers({
-  handleTransaction
+  handleTransaction,
+  getPrices
 });
 
 export default rootReducer;
